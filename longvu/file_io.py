e36fa8793d9194e71641ff9475d61d70d11dc817
XiaoqianShen <64844805+xiaoqian-shen@users.noreply.github.com>
2024-10-09 11:21:35 -0700

    Update file_io.py

--- longvu/file_io.py
+++ longvu/file_io.py
@@ -1,7 +1,3 @@
-# Copyright (c) Meta Platforms, Inc. and its affiliates.
-
-# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
-
 from iopath.common.file_io import HTTPURLHandler, PathManager as PathManagerBase
 
 __all__ = ["PathManager"]
